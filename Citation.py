29863fc7afd1a2286449c7e47feb001c7073b951
hrybacki <hrybacki@gmail.com>
2013-02-10 18:04:50 -0500

    Wasn't up to PEP standards. Needed descriptive todo for author data. RE/Pickle imports were unused.

--- Citation.py
+++ Citation.py
@@ -1,5 +1,5 @@
 import re
-import pickle
+
 
 # class representing a citation
 class Citation(dict):
@@ -11,8 +11,8 @@ class Citation(dict):
 
     # constructor
     def __init__(self, **kwargs):
-	# for every key, populate the respecitve Citation field with its value
-        for k,v in kwargs.iteritems():
+    # for every key, populate the respective Citation field with its value
+        for k, v in kwargs.iteritems():
             self[k] = v
 
     def __delitem__(self, key):
@@ -22,37 +22,36 @@ class Citation(dict):
     __getattr__ = dict.__getitem__
     __setattr__ = dict.__setitem__
 
-    # returns citation obj - sublclass of dictionary
-    # parse and seperate fields from raw data
+    # returns citation obj - subclass of dictionary
+    # parse and separate fields from raw data
     @classmethod
     def parse(cls, raw):
-	# create an empty Citation
+    # create an empty Citation
         self = cls()
         self['raw'] = raw
 
-        # seperate fields
+        # separate fields
         matches = self.rex_authors.match(self.raw)
 
         # grab authors field
         authors = matches.group('authors')
-        print authors
+
         self.parse_authors(authors)
-        #print 'Authors: ' + self.getAuthors()
         # grab date field
         issued = matches.group('date')
         self.parse_date(issued)
-        #print 'Date: ' + self.getDate()
+
         # grab postData field
         post = matches.group('postData')
         self.parse_post(post)
-        #print 'Post Data: ' + self.getPostData()
         return self
 
-    def parse_authors(self, raw_authors):
-        self['author'] = []
+    #@TODO: Need to parse string and store name in array as, family:lastname given:first/second initial
+    def parse_authors(self, authors):
+        self['author'] = authors
 
-    def parse_date(self, raw_date):
-        self['issued'] = None
+    def parse_date(self, date):
+        self['issued'] = date
 
-    def parse_post(self, raw_post):
-        self['post'] = None
+    def parse_post(self, post):
+        self['post'] = post
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import json
 import glob
 import unittest
-import re
 
 from Citation import Citation
+
 
 class CitationTest(unittest.TestCase):
     pass
@@ -12,6 +12,7 @@ class CitationTest(unittest.TestCase):
 def test_generator(a, b):
     def test(self):
         self.assertDictEqual(a, b)
+
     return test
 
 # return dictionary less the key/values we don't want to test
@@ -20,23 +21,24 @@ def clean(d):
         del d[k]
     return d
 
+
 if __name__ == '__main__':
     # for every json we have created in our tests directory
     for name in glob.glob("tests/*.json"):
         # open the json 
         with open(name) as f:
-	#    #base = re.match('tests/(.+)\.json', name).group(1)
+        #    #base = re.match('tests/(.+)\.json', name).group(1)
             citations = json.load(f)
             # for every citation in this json
-	    for citation in citations:
-                print citation 
-                print "citation\n\n"
-		# create a string identifying json file and raw citation data
-  	        test_name = 'test in {} - {}'.format(name, citation['raw'])
-                print test_name
-		print "testname\n\n"
-		# creat generator to compare Citation parse engine dictionary vs json manually edited dictionary
-	        test = test_generator(clean(Citation.parse(citation['raw'])), clean(Citation(**citation)))
-        	# @fixme: CitationTest.test_name() will run test_generator(Citation parse eng dict, json dictionary)
-	        setattr(CitationTest, test_name, test)
+            for citation in citations:
+                print(citation)
+                print("citation\n\n")
+                # create a string identifying json file and raw citation data
+                test_name = 'test in {} - {}'.format(name, citation['raw'])
+                print(test_name)
+                print("testname\n\n")
+                # creat generator to compare Citation parse engine dictionary vs json manually edited dictionary
+                test = test_generator(clean(Citation.parse(citation['raw'])), clean(Citation(**citation)))
+                # @fixme: CitationTest.test_name() will run test_generator(Citation parse eng dict, json dictionary)
+                setattr(CitationTest, test_name, test)
     unittest.main()
