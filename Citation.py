267fc8de1b5054bf2b7d321557a4246e151fc3d9
hrybacki <hrybacki@gmail.com>
2013-02-11 00:15:31 -0500

    author parsing system didn't account for multiple authors or multiple given initials.

--- Citation.py
+++ Citation.py
@@ -7,12 +7,12 @@ class Citation(dict):
     rawData = None
 
     # regex script for breaking apart fields
-    rex_authors = re.compile('(?P<authors>.*)\((?P<date>.*)\)(?P<postData>.*)', re.DOTALL)
+    rex_initial = re.compile('(?P<authors>.*)\((?P<date>.*)\)(?P<postData>.*)', re.DOTALL)
 
     # constructor
     def __init__(self, **kwargs):
     # for every key, populate the respective Citation field with its value
-        for k, v in kwargs.iteritems():
+        for k,v in kwargs.iteritems():
             self[k] = v
 
     def __delitem__(self, key):
@@ -31,12 +31,12 @@ class Citation(dict):
         self['raw'] = raw
 
         # separate fields
-        matches = self.rex_authors.match(self.raw)
+        matches = self.rex_initial.match(self.raw)
 
         # grab authors field
         authors = matches.group('authors')
-
         self.parse_authors(authors)
+
         # grab date field
         issued = matches.group('date')
         self.parse_date(issued)
@@ -48,7 +48,32 @@ class Citation(dict):
 
     #@TODO: Need to parse string and store name in array as, family:lastname given:first/second initial
     def parse_authors(self, authors):
-        self['author'] = authors
+        authorList = []
+        # (1) regex expression for grabbing 'Familyname, I1.' || 'Familyname, I1. I2.'
+        rex_author_list = re.compile('[A-Z][a-z]+\,\s[A-Z]\.\s+[A-Z]\.|[A-Z][a-z]+\,\s[A-Z]\.', re.DOTALL)
+        # (2) populate list with unique authors
+        matches = re.findall(rex_author_list, authors)
+        # (3) regex for seperateing an authors family name from his given initial(s)
+        rex_authors = re.compile('(?P<family>[A-Z][a-z]*),\s+(?P<givenList>.+\.)', re.DOTALL)
+        # (4) regex for seperating given initals
+        rex_given_list = re.compile('([A-Z])\.', re.DOTALL)
+        # (5) for each author found, disect family and given names and add to 'authors' dict
+        for author in matches:
+            # (5.1) seperate family name from given initial(s)
+            uniqueAuthor = rex_authors.match(author)
+            # (5.2) find all initials of an authors given name and populate an array with strings
+            givenMatches = re.findall(rex_given_list, uniqueAuthor.group('givenList')) 
+            # (5.3) for each given initial add it to a composite string
+            givenComposite = ''
+            for given in givenMatches:
+                givenComposite = givenComposite + ' ' + given 
+            # (5.4) strip leading whitespace
+            givenComposite = givenComposite.lstrip()
+            # (5.5) add given initials and family to dictionary
+            authorComplete = {'given' : givenComposite, 'family' : uniqueAuthor.group('family')}
+            # (5.6) append completed author to composite author dictionary
+            authorList.append(authorComplete)
+        self['author'] = authorList
 
     def parse_date(self, date):
         self['issued'] = date
