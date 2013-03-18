e40bb37e63f6940db5f7de78aab073afe4e79b7b
Ryan Freckleton <ryan.freckleton@gmail.com>
2013-03-18 13:27:10 -0700

    Removing unneeded files.

--- requestsTest.py
+++
@@ -1,8 +0,0 @@
-import pycurl
-
-c = pycurl.Curl()
-citation = 'Albertson, K., & Shore, C. (2009). Holding in mind conflicting information: Pretending, working memory, and executive control. Journal of Cognition and Development, 9, 390-410. doi: 10.1080/15248370802678240'
-c.setopt(c.URL, 'http://freecite.library.brown.edu/citations/create')
-c.setopt(c.POSTFIELDS, 'Accept=text/xml&citation=%s' % citation)
-c.perform()
-
