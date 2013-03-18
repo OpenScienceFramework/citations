e672cb61500fabbba974a6404a6888976200b6c6
Ryan Freckleton <ryan.freckleton@gmail.com>
2013-03-18 13:25:58 -0700

    Refactors old tests.

--- test_old_parser.py
+++ test_old_parser.py
@@ -6,49 +6,25 @@ import matplotlib.pyplot as plt
 
 from Citation import Citation
 
+unneeded_tags = ['raw', 'type', 'style', 'post', 'page', 'volume', 'DOI',
+                 'references', 'UID']
 
 class CheckCitation(unittest.TestCase):
     pass
 
-def test_generator(a, b):
-    """Return a dynamic test function for comparing two dictionaries."""
-    def test(self):
-        self.assertDictEqual(a, b)
-
-    return test
-
-def clean(d):
-    """Return dictionary with the key/values we don't want to test removed."""
-    for k in ['raw', 'type', 'style', 'post', 'page', 'volume', 'DOI', 'references', 'UID']:
-        del d[k]
-    return d
-
-def graph(results):
-    """Graph the resulting citation graph."""
-    g = nx.Graph()
-    for article in results:
-        g.add_node(results[article]['issued'])
-    pos = nx.shell_layout(g)
-    nx.draw(g, pos)
-    plt.show()
-
 def create_tests():
-    results = {}
     for name in glob.glob("tests/*.json"):
         with open(name) as f:
             citations = json.load(f)
-            for citation in citations:
-                test_name = 'test in {} - {}'.format(name, citation['raw'])
-                test = test_generator(clean(Citation.parse(citation['raw'])), clean(Citation(**citation)))
-                setattr(CheckCitation, test_name, test)
+        for citation in citations:
+            test_name = 'test in {} - {}'.format(name, citation['raw'])
+            a = Citation.parse(citation['raw'])
+            b = Citation(**citation)
+            for k in unneeded_tags:
+                del a[k]
+                del b[k]
+            def test(self):
+                assert a == b
+            setattr(CheckCitation, test_name, test)
 
-                temp = Citation.parse(citation['raw'])
-                tempUID = temp['UID']
-                results[tempUID] = temp
-    return results
-
-results = create_tests()
-
-if __name__ == '__main__':
-    graph(results)
-    unittest.main()
+create_tests()
